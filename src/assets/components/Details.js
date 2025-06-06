889ee1f4d56cd8c4272c0323c93584d34d1a2306
Ju-danoo7 <daviddaniel.dd007@gmail.com>
2025-06-06 11:44:13 +0100

    worked on the products page

--- src/assets/components/Details.js
+++ src/assets/components/Details.js
@@ -54,3 +54,66 @@ export const homeSamples = [
         description: "Cream cake with starwberry cream."
     }
 ]
+
+export const Produce = [
+    {
+        id: 1,
+        image: "./images/products/peanut-butter-cake.webp",
+        name: "Bread, pastry and cake",
+        description: "Cupcakes, celebration cakes, muffins, and brownies – we've got something for every occasion, including vegan, gluten-free and low fat recipes.",
+    },
+    {
+        id: 2,
+        image: "./images/products/Flatbreads-recipes.webp",
+        name: "Flatbread recipes",
+        description: "Rustle up some flatbreads and add your favourite topping for an easy supper. From cumin-spiced and seeded to halloumi flatbreads, find the recipe for you.",
+    },
+    {
+        id: 3,
+        image: "./images/products/chocolate-cake.webp",
+        name: "Ultimate chocolate cake",
+        description: "Indulge yourself with this ultimate chocolate ganache cake recipe that is beautifully moist, rich and fudgy. Perfect for a celebration or an afternoon tea",
+    },
+    {
+        id: 4,
+        image: "./images/products/pasties.webp",
+        name: "Pasty recipes",
+        description: "From traditional Cornish to a stunning tear & share picnic centrepiece, we've got a pastry parcel for every taste and occasion!",
+    },
+    {
+        id: 5,
+        image: "./images/products/Peach-tart.webp",
+        name: "Puff pastry tart recipes",
+        description: "Our crisp and flaky puff pastry tarts taste as good as they look. Choose from sweet and fruity tarte Tatins, savoury cheese, veggie bakes and more.",
+    },
+    {
+        id: 6,
+        image: "./images/products/tarte_tatin.webp",
+        name: "Puff pastry dessert recipes",
+        description: "From summery fruit tarts and stewed fruit pastries to Christmassy mince pies, these puff pastry dessert recipes will surely satisfy your sweet cravings.",
+    },
+    {
+        id: 7,
+        image: "./images/products/sausage-roll.webp",
+        name: "Sausage roll",
+        description: "Sausage rolls make a great picnic or buffet snack and are delicious either hot or cold. Wrap pork mince in buttery pastry for little parcels of perfection.",
+    },
+    {
+        id: 8,
+        image: "./images/products/cheesy_garlic_bread.webp",
+        name: "Garlic bread recipes",
+        description: "Serve garlic bread for a comforting starter or side. Whether plain or cheesy, it's a great accompaniment for lasagne, chilli con carne, salad, soups and more",
+    },
+    {
+        id: 9,
+        image: "./images/products/carrot-cake.webp",
+        name: "Easy carrot cake",
+        description: "Top this classic carrot cake with moreish cream cheese icing and chopped walnuts or pecans. Serve as a sweet treat with a cup of tea any time of the day."
+    },
+    {
+        id: 10,
+        image: "./images/products/sourdough.webp",
+        name: "Sourdough",
+        description: "Bake a fresh loaf of sourdough bread with our ultimate recipe collection, then turn it into tasty canapés or a hearty traybake."
+    }
+]
--- src/assets/components/pages/Products.jsx
+++ src/assets/components/pages/Products.jsx
@@ -1,7 +1,22 @@
+import { Produce } from "../Details"
+
 export default function Products() {
+    const Productz = Produce.map(item => <div className="shrink-0 flex bg-white flex-col gap-3 rounded-md w-45 h-full shadow-md" key={item.id}>
+        <img src={item.image} alt="pastry icon" className="h-40 shrink-0 rounded-lg" />
+        <div className="p-2 py-3">
+            <p className="font-bold text-sm">{item.name}</p>
+            <p className="text-sm line-clamp-4">{item.description}</p>
+        </div>
+    </div>)
+
     return (
         <>
-            <h1 className="font-bold text-2xl">Our Products Component</h1>
+            <div className="bg-slate-100 py-4 px-2">
+                <h1 className="font-bold text-2xl">Our Products</h1>
+                <div className="flex flex-wrap justify-center gap-3">
+                    {Productz}
+                </div>
+            </div>
         </>
     )
 }
