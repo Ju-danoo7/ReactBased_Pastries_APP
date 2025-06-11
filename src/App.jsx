649ec9587ce0da2ca227bafde9ff3c5ae1d9b330
Ju-danoo7 <daviddaniel.dd007@gmail.com>
2025-06-11 20:58:22 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router"
 import Layout from "./assets/components/Layout"
-import Home from "./assets/components/pages/Homepage"
-import Order from "./assets/components/pages/Cart"
-import Register from "./assets/components/pages/Profile"
-import Services from "./assets/components/pages/Services"
-import Products from "./assets/components/pages/Products"
+import { Suspense, lazy } from 'react';
+
+const Order = lazy(() => import('./assets/components/pages/Cart'));
+const Register = lazy(() => import('./assets/components/pages/Profile'));
+const Services = lazy(() => import('./assets/components/pages/Services'));
+const Products = lazy(() => import('./assets/components/pages/Products'));
+const Home = lazy(() => import("./assets/components/pages/Homepage"));
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />} >
-            <Route index element={<Home />} />
-            <Route path="cart" element={<Order />} />
-            <Route path="profile" element={<Register />} />
-            <Route path="products" element={<Products />} />
-            <Route path="services" element={<Services />} />
-          </Route>
-        </Routes>
+        <Suspense fallback={<div className="flex items-center justify-center h-screen">Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Layout />} >
+              <Route index element={<Home />} />
+              <Route path="cart" element={<Order />} />
+              <Route path="profile" element={<Register />} />
+              <Route path="products" element={<Products />} />
+              <Route path="services" element={<Services />} />
+            </Route>
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </>
   )
